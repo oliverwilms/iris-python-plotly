b58f72dc5ad9b0fa296a02756dbf61730ec9abae
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2026-07-30 19:10:15 -0500

    Update iris_script.py

--- iris_script.py
+++ iris_script.py
@@ -15,8 +15,8 @@ iris.cls('Security.Users').UnExpireUserPasswords("*")
 iris.system.Process.SetNamespace("IRISAPP")
 
 # load ipm package listed in module.xml
-#iris.cls('%ZPM.PackageManager').Shell("load /home/irisowner/dev -v")
-assert ipm('load /home/irisowner/dev -v')
+iris.cls('%ZPM.PackageManager').Shell("load /home/irisowner/dev -v")
+#assert ipm('load /home/irisowner/dev -v')
 
 # load demo data
 engine = create_engine('iris+emb:///')
